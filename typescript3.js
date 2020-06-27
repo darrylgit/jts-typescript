0f67b52893646efd8204ee43195ac074c5ecb26c
darrylgit <darrylsmithup@gmail.com>
2020-06-27 10:43:06 -0400

    Tuples and enums

--- typescript3.js
+++ typescript3.js
@@ -15,3 +15,15 @@ var wizard = {
 // null and undefined
 var meh = undefined;
 var noo = null;
+// tuple
+var basket;
+basket = ['basketball', 5];
+// Enum
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+var sizeName = Size[2];
+var sizeValue = Size.Small;
--- typescript3.ts
+++ typescript3.ts
@@ -20,3 +20,16 @@ let wizard: object = {
 // null and undefined
 let meh: undefined = undefined;
 let noo: null = null;
+
+// tuple
+let basket: [string, number];
+basket = ['basketball', 5];
+
+// Enum
+enum Size {
+  Small = 1,
+  Medium = 2,
+  Large = 3
+}
+let sizeName: string = Size[2];
+let sizeValue: number = Size.Small;
