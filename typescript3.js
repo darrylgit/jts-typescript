26d444f53c4929a36e3cfa081aee1452d67ff44d
darrylgit <darrylsmithup@gmail.com>
2020-06-27 10:37:58 -0400

    Basic types

---
+++ typescript3.js
@@ -0,0 +1,17 @@
+// boolean
+var isCool = true;
+// number
+var age = 56;
+// string
+var eyeColor = 'brown';
+var favoriteQuote = "I'm not old; I'm only " + age;
+// Array
+var pets = ['cat', 'dog', 'pig'];
+var pets2 = ['lion', 'tiger', 'bear'];
+// Object
+var wizard = {
+    a: 'John'
+};
+// null and undefined
+var meh = undefined;
+var noo = null;
---
+++ typescript3.ts
@@ -0,0 +1,22 @@
+// boolean
+let isCool: boolean = true;
+
+// number
+let age: number = 56;
+
+// string
+let eyeColor: string = 'brown';
+let favoriteQuote: string = `I'm not old; I'm only ${age}`;
+
+// Array
+let pets: string[] = ['cat', 'dog', 'pig'];
+let pets2: Array<string> = ['lion', 'tiger', 'bear'];
+
+// Object
+let wizard: object = {
+  a: 'John'
+};
+
+// null and undefined
+let meh: undefined = undefined;
+let noo: null = null;
